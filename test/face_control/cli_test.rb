cc48c0dd76cd94b17f6cce335f3533ff31a606b8
Ilya Vassilevsky <vassilevsky@gmail.com>
2015-08-09 11:38:09 +0300

    Extract main method to a separate class and test it

---
+++ test/face_control/cli_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class CLITest < Minitest::Test
+  def test_check
+    pull_request = OpenStruct.new(filenames_with_added_lines: %w(test/fixtures/foo.rb test/fixtures/foo.coffee))
+    logger = Logger.new('/dev/null')
+    comments = FaceControl::CLI.new.check(pull_request, %w(foo), logger)
+    assert_equal 2, comments.size
+  end
+end
